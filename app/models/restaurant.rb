9fd31dee85d64d70193aa7b0532b5d70f353d63a
jeff41101 <jeff41101@yahoo.com.tw>
2021-03-26 16:42:49 +0800

    styled restaurant show page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
-  validates_presence_of :name
+  validates_presence_of :name, :opening_hours, :tel, :address, :description, :image
 end
