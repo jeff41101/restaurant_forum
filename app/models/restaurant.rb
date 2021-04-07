bdad00a3b0150264f91bbf56cddedf0c3556dbf8
jeff41101 <jeff41101@yahoo.com.tw>
2021-04-07 16:19:20 +0800

    add route for farvorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,13 @@
 class Restaurant < ApplicationRecord
   #設定關聯
   belongs_to :category
-  
+
   # 如果刪除Restaurant也會一併刪除Comment
   has_many :comments, dependent: :destroy
   mount_uploader :image, PhotoUploader
+  
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   
   #確認資料有填寫
   validates_presence_of :name, :opening_hours, :tel, :address, :description
